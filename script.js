0c93c37835ae6cda2bf3fa037368733f32bb651f
loukya-7 <131159563+loukya-7@users.noreply.github.com>
2023-06-30 20:53:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// JavaScript
+window.addEventListener('DOMContentLoaded', function() {
+  const browserInfoDiv = document.getElementById('browser-info');
+  const browserName = navigator.userAgent;
+  const version = parseFloat(navigator.appVersion);
+  const message = "You are using " + browserName + " version " + version;
+  browserInfoDiv.textContent = message;
+});
+
